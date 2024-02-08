2a729ab27e338947b8eaab9ce2b93c901754ade1
Stanislav Vassiljev <stanvas@gmail.com>
2024-02-08 13:42:52 +0200

    request error handling

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -2,11 +2,11 @@ import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
-import { errorHandler } from "./middleware/ErrorHandler";
 import { currentUserRouter } from "./route/CurrentUser";
 import { loginRouter } from "./route/LogIn";
 import { logoutRouter } from "./route/LogOut";
 import { newUser } from "./route/NewUser";
+import { errorHandler } from "@betstan/common";
 
 const app = express();
 app.set("trust proxy", true);
