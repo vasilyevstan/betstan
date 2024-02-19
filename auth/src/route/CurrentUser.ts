30e5c8e3aff4db0ecebb1e3bae2638cd78d34ae3
Stanislav Vassiljev <stanvas@gmail.com>
2024-02-19 12:52:37 +0200

    session expires in 12 hours

--- auth/src/route/CurrentUser.ts
+++ auth/src/route/CurrentUser.ts
@@ -1,10 +1,31 @@
 import express from "express";
 import { currentUser } from "../middleware/CurrentUser";
+import { User } from "../model/User";
 
 const router = express.Router();
 
-router.get("/api/auth/currentuser", currentUser, (req, res) => {
-  res.send({ currentUser: req.currentUser || null });
+router.get("/api/auth/currentuser", currentUser, async (req, res) => {
+  let currentUser = req.currentUser;
+
+  if (currentUser) {
+    const user = await User.findById(currentUser.id);
+
+    // session token expires evert 12 hours
+    const dateToExpire = currentUser.timestamp
+      ? new Date(
+          new Date(currentUser.timestamp).getTime() + 12 * 60 * 60 * 1000
+        )
+      : new Date().getTime() - 1 * 60 * 60 * 1000;
+
+    if (!user || new Date() > dateToExpire) {
+      console.log("user not found or expired", user, currentUser.email);
+
+      req.session = null;
+      currentUser = undefined;
+    }
+  }
+
+  res.send({ currentUser: currentUser || null });
 });
 
 export { router as currentUserRouter };
