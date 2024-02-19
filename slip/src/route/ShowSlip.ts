40f645e807c3723f8fd7c75c290bcc6defb80338
Stanislav Vassiljev <stanvas@gmail.com>
2024-02-19 13:29:14 +0200

    sending status in case of an error correctly

--- slip/src/route/ShowSlip.ts
+++ slip/src/route/ShowSlip.ts
@@ -5,17 +5,13 @@ import { SlipStatus } from "@betstan/common";
 const router = express.Router();
 
 router.get("/api/slip", async (req: Request, res: Response) => {
-  // console.log("fetching slip");
-
   if (!req.currentUser) {
-    // console.log("returning empty slip");
     res.send({});
   } else {
     const slip = await Slip.findOne({
       userId: req.currentUser.id,
       status: SlipStatus.DRAFT,
     });
-    // console.log("returning slip", slip);
     res.send(slip);
   }
 });
