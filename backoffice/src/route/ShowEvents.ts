b27816b7eabbf0453efbbcd500beb90464a9ff3c
Stanislav Vassiljev <stanvas@gmail.com>
2024-03-15 16:18:20 +0200

    grouping by status as well

--- backoffice/src/route/ShowEvents.ts
+++ backoffice/src/route/ShowEvents.ts
@@ -4,7 +4,7 @@ import { Event } from "../model/Event";
 const router = express.Router();
 
 router.get("/api/backoffice", async (req: Request, res: Response) => {
-  const events = await Event.find().sort({ time: 1 });
+  const events = await Event.find().sort({ status: 1, time: 1 });
 
   res.send(events);
 });
